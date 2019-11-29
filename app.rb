d51bd64cd9ed224f5782a70dc02ddd02ee07e5a4
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-29 03:28:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,16 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
-
-end
+	
+	get '/' do
+	  erb :hello
+  end 
+  
+  get '/'
+  erb :goodbye
+  end 
+  
+  get '/'
+  erb :date
+  end
+end 
